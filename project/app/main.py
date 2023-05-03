3819ef473e1edbd448217aba2035b9352971543c
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-03 15:11:45 +0200

    Add Postgres and Tortoise

--- project/app/main.py
+++ project/app/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI, Depends
-from app.config import get_settings, Settings
+import os
+
+from app.config import Settings, get_settings
+from fastapi import Depends, FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
 def pong(settings: Settings = Depends(get_settings)):
-    return {"ping": "pong",
-            "environment": settings.environment,
-            "testing": settings.testing
-            }
+    return {"ping": "pong", "environment": settings.environment, "testing": settings.testing}
